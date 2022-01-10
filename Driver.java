814260f8b8176188d10f7371a51b4abb8d860f6c
courtneyhg <chuang31@stuy.edu>
2022-01-10 17:04:29 -0500

    tied up loose ends

--- Driver.java
+++ Driver.java
@@ -112,6 +112,18 @@ public class Driver {
     a.counter = 0;
     a.swaps = 0;
 
+    System.out.println("\nWORST CASE OF BUBBLE!");
+    ArrayList ws = new ArrayList<Integer>();
+    for (int bib = 100; bib > 0; bib--) {
+      ws.add(bib);
+    }
+    System.out.println("Arraylist hell before sorting:\n" + ws);
+    a.insertion(ws);
+    System.out.println("ArrayList hell after sorting:\n" + ws);
+    System.out.println("Selection passes: " + a.counter);
+    System.out.println("Selection swaps: " + a.swaps);
+    a.counter = 0;
+    a.swaps = 0;
 
 
     System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
@@ -187,9 +199,9 @@ public class Driver {
     for (int bib = 100; bib > 0; bib--) {
       hell.add(bib);
     }
-    System.out.println("Arraylist biden before sorting:\n" + hell);
+    System.out.println("Arraylist hell before sorting:\n" + hell);
     b.insertion(hell);
-    System.out.println("ArrayList biden after sorting:\n" + hell);
+    System.out.println("ArrayList hell after sorting:\n" + hell);
     System.out.println("Selection passes: " + b.counter);
     System.out.println("Selection swaps: " + b.swaps);
     b.counter = 0;
@@ -226,12 +238,12 @@ public class Driver {
     kol.add(5);
     kol.add(6);
     System.out.println("ArrayList kol before sorting:\n" + kol);
-    b.insertion(kol);
+    c.insertion(kol);
     System.out.println("ArrayList kol after sorting:\n" + kol);
-    System.out.println("Selection passes: " + b.counter);
-    System.out.println("Selection swaps: " + b.swaps);
-    b.counter = 0;
-    b.swaps = 0;
+    System.out.println("Selection passes: " + c.counter);
+    System.out.println("Selection swaps: " + c.swaps);
+    c.counter = 0;
+    c.swaps = 0;
 
     System.out.println("\nBEST CASE OF INSERTION!");
     ArrayList qq = new ArrayList<Integer>();
@@ -242,12 +254,12 @@ public class Driver {
     qq.add(6);
     qq.add(6);
     System.out.println("ArrayList qq before sorting:\n" + qq);
-    b.insertion(qq);
+    c.insertion(qq);
     System.out.println("ArrayList qq after sorting:\n" + qq);
-    System.out.println("Selection passes: " + b.counter);
-    System.out.println("Selection swaps: " + b.swaps);
-    b.counter = 0;
-    b.swaps = 0;
+    System.out.println("Selection passes: " + c.counter);
+    System.out.println("Selection swaps: " + c.swaps);
+    c.counter = 0;
+    c.swaps = 0;
 
     System.out.println("\nWORST CASE OF INSERTION!");
     ArrayList wer = new ArrayList<Integer>();
@@ -260,10 +272,10 @@ public class Driver {
     System.out.println("ArrayList wer before sorting:\n" + wer);
     b.insertion(wer);
     System.out.println("ArrayList wer after sorting:\n" + wer);
-    System.out.println("Selection passes: " + b.counter);
-    System.out.println("Selection swaps: " + b.swaps);
-    b.counter = 0;
-    b.swaps = 0;
+    System.out.println("Selection passes: " + c.counter);
+    System.out.println("Selection swaps: " + c.swaps);
+    c.counter = 0;
+    c.swaps = 0;
 
     System.out.println("\nWORST CASE OF INSERTION!");
     ArrayList oop = new ArrayList<Integer>();
@@ -280,12 +292,12 @@ public class Driver {
     oop.add(-40);
     oop.add(-50);
     System.out.println("ArrayList oop before sorting:\n" + oop);
-    b.insertion(oop);
+    c.insertion(oop);
     System.out.println("ArrayList oop after sorting:\n" + oop);
-    System.out.println("Selection passes: " + b.counter);
-    System.out.println("Selection swaps: " + b.swaps);
-    b.counter = 0;
-    b.swaps = 0;
+    System.out.println("Selection passes: " + c.counter);
+    System.out.println("Selection swaps: " + c.swaps);
+    c.counter = 0;
+    c.swaps = 0;
 
     System.out.println("\nWORST CASE OF INSERTION!");
     ArrayList biden = new ArrayList<Integer>();
@@ -293,12 +305,12 @@ public class Driver {
       biden.add(bib);
     }
     System.out.println("Arraylist biden before sorting:\n" + biden);
-    b.insertion(biden);
+    c.insertion(biden);
     System.out.println("ArrayList biden after sorting:\n" + biden);
-    System.out.println("Selection passes: " + b.counter);
-    System.out.println("Selection swaps: " + b.swaps);
-    b.counter = 0;
-    b.swaps = 0;
+    System.out.println("Selection passes: " + c.counter);
+    System.out.println("Selection swaps: " + c.swaps);
+    c.counter = 0;
+    c.swaps = 0;
   } //end main
 
 } //end class
